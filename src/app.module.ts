5c0f81548a7a4a32a2fa9ea50880710abb77ba0e
Serhii Olabin <serhii.olabin@solarwinds.com>
2021-03-19 22:10:07 +0200

    added some resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './resources/cats/cats.module';
+import { GameModule } from './resources/game/game.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
